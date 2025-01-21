51a9cfd6c66823d645c0d39808fae79f417c1ceb
AlanGdev <alan.grolleau@gmail.com>
2025-01-21 22:22:59 +0100

    /controller: user.js: implémentation de la logique métier pour la fonction login - Ajout du package jsonwebtoken pour génération d'un token /middelware: creéation du fichier auth pour vérification token Ajout de cette fonction aux routes en ayant besoin.

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.userSignup = (req, res, next) => {
 	//res.status(201).json({ message: 'requête POST sur fonction userSignup'
@@ -19,5 +20,34 @@ exports.userSignup = (req, res, next) => {
 };
 
 exports.userLogin = (req, res, next) => {
-	res.status(201).json({ message: 'requête POST sur fonction userLogin' });
+	//res.status(201).json({ message: 'requête POST sur fonction userLogin' });
+	User.findOne({ email: req.body.email })
+		.then((user) => {
+			if (!user) {
+				return res
+					.status(401)
+					.json({ message: 'Paire login / mot de passe incorrecte' });
+			}
+			bcrypt
+				.compare(req.body.password, user.password)
+				.then((validation) => {
+					if (!validation) {
+						return res
+							.status(401)
+							.json({ message: 'Paire login / mot de passe incorrecte' });
+					}
+					res.status(201).json({
+						userId: user._id,
+						token: jwt.sign(
+							{
+								userId: user._id,
+							},
+							'RANDOM_TOKEN_SECRET',
+							{ expiresIn: '24h' },
+						),
+					});
+				})
+				.catch((error) => res.status(500).json({ error }));
+		})
+		.catch((error) => res.status(404).json({ message: 'user non trouvé' }));
 };
--- routes/book.js
+++ routes/book.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bookCtrl = require('../controllers/book');
+const auth = require('../middelware/auth');
 
 const router = express.Router();
 
 router.get('/', bookCtrl.getBooks);
 router.get('/bestrating', bookCtrl.getBestRating);
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', bookCtrl.createBook);
-router.put('/:id', bookCtrl.modifyBook);
-router.delete('/:id', bookCtrl.deleteBook);
-router.post('/:id/rating', bookCtrl.rateBook);
+router.post('/', auth, bookCtrl.createBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+router.post('/:id/rating', auth, bookCtrl.rateBook);
 module.exports = router;
